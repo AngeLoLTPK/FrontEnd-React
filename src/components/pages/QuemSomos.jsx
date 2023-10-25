feb2071a8f7acfd5df6ea10d4eacf386f39771e5
angelo <angelogm2005@gmail.com>
2023-10-25 13:57:54 -0300

    Update funcionando agora falta fazer o componente recarregar

--- src/components/pages/QuemSomos.jsx
+++ src/components/pages/QuemSomos.jsx
@@ -9,29 +9,12 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { Link } from "react-router-dom";
-import { BiEdit as IconEdit } from "react-icons/bi"
-import { FaTrash as Lixeira } from "react-icons/Fa"
+import CardUser from "../ui/CardUser.jsx";
 
 export const QuemSomos = () => {
 
   let [users, setUsers] = useState([]);
   let [showModal, setShowModal] = useState(false);
-  let [showEditModal, setShowEditModal] = useState(false);
-
-
-  const [name, setName] = useState(users.name)
-  const [email, setEmail] = useState(users.email)
-  const [photo, setPhoto] = useState(users.photo)
-
-  const User = users.map((userC) => {
-    if (userC.id == users.id) {
-      return data.users
-    } else {
-      return userC
-    }
-  })
-
-  console.log(user.id)
 
 
   useEffect(() => {
@@ -39,11 +22,9 @@ export const QuemSomos = () => {
     const getUsers = async () => {
       const response = await fetch("http://localhost:3000/user/list")
       const data = await response.json()
-      if (response.ok) {
-        setUsers(data.users)
-      } else {
-        setUsers(false)
-      }
+      console.log(data.success)
+      console.log(data.users)
+      setUsers(data.users)
     };
 
     getUsers()
@@ -72,67 +53,67 @@ export const QuemSomos = () => {
     if (response.ok) {
       const data = await response.json()
       alert(data?.success)
-
-
+      setShowModal(false)
+      setUsers([...users, data.user])
     }
 
   };
 
-  const handlePut = async (event) => {
-    event.preventDefault()
-    const newUser = {
-      name: name,
-      email: email,
-      photo: photo,
-      id: users.id
-    }
-    const response = await fetch('http://localhost:3000/user', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newUser)
-    })
+  //   const handlePut = async (event) => {
+  //     event.preventDefault()
+  //     const newUser = {
+  //       name: name,
+  //       email: email,
+  //       photo: photo,
+  //       id: users.id
+  //     }
+  //     const response = await fetch('http://localhost:3000/user', {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(newUser)
+  //     })
 
-      console.log(id)
+  //       console.log(id)
 
-    if (response.ok) {
-      const data = await response.json()
-      alert(data.success)
-      setShowEditModal(false)
-      const newUsers = users.map((userC) => {
-        if (userC.id == users.id) {
-          return data.users
-        } else {
-          return userC
-        }
-      })
-      setUsers(newUsers)
-    }
+  //     if (response.ok) {
+  //       const data = await response.json()
+  //       alert(data.success)
+  //       setShowEditModal(false)
+  //       const newUsers = users.map((userC) => {
+  //         if (userC.id == users.id) {
+  //           return data.users
+  //         } else {
+  //           return userC
+  //         }
+  //       })
+  //       setUsers(newUsers)
+  //     }
 
-  }
+  //   }
 
-  const HandleDelete = async (id) => {
+  // const HandleDelete = async (id) => {
 
-    const response = await fetch('http://localhost:3000/user', {
-      method: 'Delete',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ id: id })
-    })
+  //   const response = await fetch('http://localhost:3000/user', {
+  //     method: 'Delete',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ id: id })
+  //   })
 
-    if (response.ok) {
-      const data = await response.json()
-      alert(data.success)
-      setShowModal(false)
-      setUsers([...users, data.user])
-      const newUsers = users.filter((user) => user.id != id)
-      setUsers(newUsers)
-    }
+  //   if (response.ok) {
+  //     const data = await response.json()
+  //     alert(data.success)
+  //     setShowModal(false)
+  //     setUsers([...users, data.user])
+  //     const newUsers = users.filter((user) => user.id != id)
+  //     setUsers(newUsers)
+  //   }
 
 
-  }
+  // }
 
 
 
@@ -198,66 +179,12 @@ export const QuemSomos = () => {
           {
             users === false ? <p>Nenhum usuário cadastrado...</p> : users.length > 0 ? users.map((user) => {
 
-              return (
-                <Card key={user.id} style={{ width: '18rem', margin: "15px", position: "relative" }}>
-                  <Card.Img variant="top" src={user.photo} />
-                  <Card.Body>
-                    <Card.Title>{user.name}</Card.Title>
-                    <Card.Text> ID: {user.id}  </Card.Text>
-                    <Card.Text>  {user.email} </Card.Text>
-
-        
-                      <IconEdit style={{ width: "45px", position: "absolute", bottom: "110px", right: "40px", color: "green" }} onClick={() => setShowEditModal(true)} />
-              
-
-                    <Lixeira style={{ position: "absolute", bottom: "110px", right: "10px", width: "45px", color: "red" }} onClick={() => HandleDelete(user.id)} />
-                  </Card.Body>
-                </Card>
-              );
+              return <CardUser key={user.id} user={user} users={users} setUsers={setUsers} />
 
               //  return <CardUser user={user} />
 
             }) : <p>Loading...</p>
           }
-
-
-          {/* modal edit  */}
-
-          <Modal
-            show={showEditModal}
-            onHide={() => setShowEditModal(false)}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id="contained-modal-title-vcenter">
-                Editar Usuário
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form onSubmit={handlePut}>
-                <Form.Group className="mb-3">
-                  <Form.Label>Nome</Form.Label>
-                  <Form.Control type="text" name="name" value={name} onChange={(event) => setName(event.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                  <Form.Label>URL Foto</Form.Label>
-                  <Form.Control type="text" name="photo" value={photo} onChange={(event) => setPhoto(event.target.value)} />
-                </Form.Group>
-                <Button variant="primary" type="submit">Editar</Button>
-              </Form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={() => setShowEditModal(false)}>Close</Button>
-            </Modal.Footer>
-
-          </Modal>
-
 
         </Content>
       </div>
--- src/components/ui/CardUser.jsx
+++ src/components/ui/CardUser.jsx
@@ -1,14 +1,142 @@
 import './CardUser.css'
+import { useEffect, useState } from "react";
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button'
+import { BiEdit as IconEdit } from "react-icons/bi"
+import { FaTrash as IconTrash } from "react-icons/Fa"
 
-const CardUser = ({user}) => {
+const CardUser = ({ user, users, setUsers }) => {
+
+  const [edit, setEdit] = useState([]);
+  const [showEditModal, setShowEditModal] = useState(false);
+
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [photo, setPhoto] = useState(user.photo);
+
+
+
+  const handlePut = async (event) => {
+    event.preventDefault()
+    const newUser = {
+      id: user.id,
+      name: name,
+      email: email,
+      photo: photo
+    }
+    const response = await fetch('http://localhost:3000/user', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+
+    if (response.ok) {
+      const data = await response.json()
+      alert(data.success)
+      setShowEditModal(false)
+      const newUsers = users.map((userC) => {
+        if (userC.id == users.id) {
+          return data.users
+        } else {
+          return userC
+        }
+      })
+      setEdit([edit, newUsers])
+      setUsers(newUsers)
+    }
+
+  }
+
+  const HandleDelete = async (id) => {
+
+    const response = await fetch('http://localhost:3000/user', {
+      method: 'Delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id: id })
+    })
+
+    if (response.ok) {
+      const data = await response.json()
+      alert(data.success)
+      setShowModal(false)
+      setUsers([...users, data.user])
+      const newUsers = users.filter((user) => user.id != id)
+      setUsers(newUsers)
+    }
+
+
+  }
+
   return (
-    <div className="card-user">
-        <img src={user.photo} alt={user.nome} />
+    <>
+      {/* <div className="card-user"> */}
+        {/* <img src={user.photo} alt={user.name} /> */}
         <div>
-            <h3>{user.name}</h3>
+          {/* <h3>{user.name}</h3>
             <span>{user.email}</span>
+            <IconEdit className='icon-edit' onClick={() => setShowEditModal(true)} />
+            <IconTrash className='icon-trash' onClick={() => handleDelete(user.id)} /> */}
+
+
+          <Card key={user.id} style={{ width: '18rem', margin: "15px", position: "relative" }}>
+            <Card.Img variant="top" src={user.photo} />
+            <Card.Body>
+              <Card.Title>{user.name}</Card.Title>
+              <Card.Text>  {user.email} </Card.Text>
+
+
+              <IconEdit style={{ width: "45px", position: "absolute", bottom: "80px", right: "40px", color: "green" }} onClick={() => setShowEditModal(true)} />
+
+
+              <IconTrash style={{ position: "absolute", bottom: "80px", right: "10px", width: "45px", color: "red" }} onClick={() => HandleDelete(user.id)} />
+            </Card.Body>
+          </Card>
+
+
         </div>
-    </div>
+      {/* </div> */}
+      <Modal
+        show={showEditModal}
+        onHide={() => setShowEditModal(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Editar Usuário
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handlePut}>
+            <Form.Group className="mb-3">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control type="text" name="name" value={name} onChange={(event) => setName(event.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>URL Foto</Form.Label>
+              <Form.Control type="text" name="photo" value={photo} onChange={(event) => setPhoto(event.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit">Editar</Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setShowEditModal(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
 
